e5bf9d9995103975a983424717d3f643d1413751
epstlight <heewkk@gmail.com>
2020-05-06 17:15:16 +0900

    remove

--- src/main/java/com/ep/jo/controller/HelloController.java
+++ src/main/java/com/ep/jo/controller/HelloController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/v1")
-@RequiredArgsConstructor
 public class HelloController {
 	
 	@GetMapping
