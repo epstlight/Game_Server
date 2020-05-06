b49e1019ff248ab9c376dc4fcf398b95cc763255
epstlight <heewkk@gmail.com>
2020-05-06 14:25:55 +0900

    git init

---
+++ src/main/java/com/ep/jo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ep.jo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		return "hello";
+	}
+}
