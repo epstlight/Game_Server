0b07d842a1f31a3d8652904bd0cfc7ee1fbca929
epstlight <heewkk@gmail.com>
2020-05-11 17:36:00 +0900

    complete userInfo

--- src/main/java/com/ep/jo/GameDemoApplication.java
+++ src/main/java/com/ep/jo/GameDemoApplication.java
@@ -2,8 +2,10 @@ package com.ep.jo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GameDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ep/jo/config/WebSocketConfig.java
+++ src/main/java/com/ep/jo/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.ep.jo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
