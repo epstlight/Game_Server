8f134b311c3b6f75a58e11b4d10c124199591ed0
epstlight <heewkk@gmail.com>
2020-05-12 22:31:53 +0900

    test11

--- src/main/java/com/ep/jo/config/SecurityConfig.java
+++ src/main/java/com/ep/jo/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
 				.csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리.
 				.authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-					.antMatchers("/user/**").hasRole("ADMIN")
+//					.antMatchers("/user/**").hasRole("ADMIN")
 					.antMatchers("/**").permitAll()
 				.and()
 					.formLogin()
